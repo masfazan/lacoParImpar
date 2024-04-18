bf9a024d131ad28e1522c4e44c23c20333826f95
masfazan <msfazan@icloud.com>
2024-04-18 16:04:50 -0300

    Add project files.

---
+++ lacoParImpar/Program.cs
@@ -0,0 +1,20 @@
+﻿int inicial = 0, final = 0, aux = 0;
+    
+Console.WriteLine("Digite um numero inicial: ");
+inicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um numero final: ");
+final = int.Parse(Console.ReadLine());
+
+aux = inicial;
+for (int i = inicial+1; i < final; i++){
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i} é par!");
+    }
+    else
+    {
+        Console.WriteLine($"{i} impar!");
+    }
+}
+
